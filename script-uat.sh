223c6eb19d2eff77388a79ba989884b1907ec1aa
Jeremy León <jeremy.leon.g@outlook.com>
2025-11-17 18:48:25 -0500

    Se modifican los scripts

---
+++ script-uat.sh
@@ -0,0 +1,19 @@
+# Obtenemos el listado de servidores de BD de desarrollo
+az_servers=$(az postgres flexible-server list --output json --query "[? contains(resourceGroup,'UAT')].{NAME_SERVER: name, RESOURCE_GROUP: resourceGroup}")
+    for row in $(echo "${az_servers}" | jq -r '.[] | @base64'); do
+        _jq() {
+            echo ${row} | base64 -d | jq -r ${1}
+        }
+
+        name_server=$(_jq '.NAME_SERVER')
+        resource_group=$(_jq '.RESOURCE_GROUP')
+
+        echo "Iniciando el servidor: ${name_server} en el grupo de recursos: ${resource_group}"
+
+        # Ejecuta el comando para iniciar el servidor
+        az postgres flexible-server start --resource-group "${resource_group}" --name "${name_server}" --no-wait
+        echo "======================================================================================================================"
+        echo "El servidor ${name_server} ha sido iniciado."
+        echo "======================================================================================================================"
+    done
+
